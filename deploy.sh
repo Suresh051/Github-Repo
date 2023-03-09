b0aa0f78ab98769d5440a0070a93a1819517e841
Suresh051 <sureshreddy061984@gmail.com>
2023-03-09 15:07:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
-L1=$1
+L=$1
 
-if [ -z "$L1" ]; then
+if [ -z "$L" ]; then
   echo "Provide Servers name"
   exit 1
 fi
-for n in $(cat $L1.txt)
+for n in $(cat $L.txt)
 do
 echo $(date)
+echo "Env is $L"
 echo "Server IP: $n"
 
 done

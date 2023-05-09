392e1908a590dd0aefb092857417895f8f5e501a
Suresh051 <sureshreddy061984@gmail.com>
2023-05-09 19:41:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch scenario.txt
 ls -lrt
-find ./src/it/resources/scenarios -type f -exec basename {} \\; >> scenario.txt
+find ./src/it/resources/scenarios -type f -exec basename {} \; >> scenario.txt
